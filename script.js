b03b4e8379f5656a9e22bc7d8e96f2240e222f75
dillonqa <dillonqa@gmail.com>
2026-04-07 09:08:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const body = document.body;
-  const currentPage = body.getAttribute("data-page");
+  const currentPage = document.body.getAttribute("data-page");
   const navLinks = document.querySelectorAll(".sidebar-nav a");
   const menuToggle = document.getElementById("menuToggle");
   const sidebarNav = document.querySelector(".sidebar-nav");
